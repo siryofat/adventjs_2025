5ec78e2e0ca36c050e9f50920655d838800fe733
sirYofat <siryofat@gmail.com>
2025-12-07 06:55:30 +0100

    feat: day05 solved

--- python/reto05.py
+++ python/reto05.py
@@ -1,6 +1,21 @@
-def main():
-    print("Hello from adventjs2025!")
+take_off = "2025*12*25@00|00|00 NP"
+
+from_times = [
+    "2025*12*24@23|59|30 NP",
+    "2025*12*25@00|00|00 NP",
+    "2025*12*25@00|00|12 NP",
+]
 
 
-if __name__ == "__main__":
-    main()
+def time_until_take_off(from_time: str, take_off_time: str) -> int:
+    from datetime import datetime
+
+    parse_string = "%Y*%m*%d@%H|%M|%S NP"
+    dt_from = datetime.strptime(from_time, parse_string)
+    dt_take_off = datetime.strptime(take_off_time, parse_string)
+    delta = dt_take_off - dt_from
+    return int(delta.total_seconds())
+
+
+for from_time in from_times:
+    print(time_until_take_off(from_time, take_off))
