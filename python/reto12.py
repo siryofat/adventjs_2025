bfa84b7b79d9eef6b235031a9e404a1eb7c62755
sirYofat <siryofat@gmail.com>
2025-12-13 06:34:55 +0100

    feat: solve day12 python

---
+++ python/reto12.py
@@ -0,0 +1,43 @@
+tests = [
+    ("A", "B", 0),
+    ("F", "B", 1),
+    ("AAB", "BBA", 0),
+    ("AFA", "BBA", 1),
+    ("AFAB", "BBAF", 1),
+    ("AA", "FF", 2),
+]
+
+
+def elf_battle(elf1: str, elf2: str) -> int:
+    RULES = {
+        "A": {"A": (1, 1), "B": (0, 0), "F": (2, 1)},
+        "B": {"A": (0, 0), "B": (0, 0), "F": (2, 0)},
+        "F": {"A": (1, 2), "B": (0, 2), "F": (2, 2)},
+    }
+
+    hp1 = 3
+    hp2 = 3
+
+    for atk1, atk2 in zip(elf1, elf2):
+        dmg1, dmg2 = RULES[atk1][atk2]
+        hp1 -= dmg1
+        hp2 -= dmg2
+        if hp1 <= 0 and hp2 <= 0:
+            return 0
+        elif hp1 <= 0:
+            return 2
+        elif hp2 <= 0:
+            return 1
+
+    if hp1 == hp2:
+        return 0
+    elif hp1 < hp2:
+        return 2
+    else:
+        return 1
+
+
+for test in tests:
+    elf1, elf2, expected = test
+    output = elf_battle(elf1, elf2)
+    print(f"{output=}, {expected=}, {'ok' if output == expected else '<--------'}")
